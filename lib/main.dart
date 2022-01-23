3895002ca73e08998a37b3c490a978beaaececd7
ideal_lust3r <45708630+llimental@users.noreply.github.com>
2022-01-23 23:43:11 +0900

    7 Button
    Multiple Sounds, func, background

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,83 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber){ // input값 사용
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: (){
-                final player = AudioCache(); // local variable
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red)
+                ),
+                onPressed: () {
+                 playSound(1); // 값 전달
+                },
+                child: Container(), // TextButton은 child가 필요함
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.orange)
+                ),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow)
+                ),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
+                ),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.teal)
+                ),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)
+                ),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.purple)
+                ),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Container(),
+              ),
+            ],
           ),
         ),
       ),
